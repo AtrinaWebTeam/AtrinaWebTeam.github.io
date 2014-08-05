6e59b634a69256bb640f1526b1c485f41ac8b3ea
k00lagin <k00lagin@gmail.com>
2014-08-06 00:16:49 +0600

    next photo on click

--- js/js.js
+++ js/js.js
@@ -14,7 +14,7 @@
 			if (anchor.getAttribute('href') && (relAttribute.toLowerCase().match('mediaphoto')))
 			{
 				photoQueue.push(anchor.href);
-				var queuePos = photoQueue.length;
+				var queuePos = photoQueue.length-1;
 				anchor.rel = queuePos;
 				anchor.onclick = function () {showPhoto(this);}
 				anchor.removeAttribute('href');
@@ -26,18 +26,46 @@
 		if (photoQueue==undefined)
 		{
 			return;
-		}		
+		}
 		openMedia();
-		var photoFrame = new Image();
-		photoFrame.onload = function(){photoFrame.style.visibility = 'visible';}
-		photoFrame.src = photoQueue[photo.rel-1];
+		currentPos = parseInt(photo.rel, 10);
+		if (photoPreload[currentPos]==undefined)
+		{
+			preloadPhoto(currentPos);
+		}
 		if (contentBox==undefined)
 		{
 			var contentBox = document.getElementById('content-box');
 		}
-		contentBox.appendChild(photoFrame);
-		document.getElementById('mediaTitle').innerHTML = 'Фотография '+photo.rel+' из '+photoQueue.length;
-
+		contentBox.appendChild(photoPreload[currentPos]);
+		document.getElementById('mediaTitle').innerHTML = 'Фотография '+(currentPos+1)+' из '+photoQueue.length;
+		preloadPhoto(currentPos+1);
+	}
+	function preloadPhoto(number)
+	{
+		if (photoQueue[number])
+		{
+			photoPreload[number] = new Image();
+			photoPreload[number].onload = function(){photoPreload[number].style.visibility = 'visible';}
+			photoPreload[number].onclick = function(){nextPhoto();}
+			photoPreload[number].src = photoQueue[number];
+		}
+	}
+	function nextPhoto()
+	{
+		currentPos = ((currentPos + 1)%photoQueue.length);
+		if (photoPreload[currentPos]==undefined)
+		{
+			preloadPhoto(currentPos);
+		}
+		if (contentBox==undefined)
+		{
+			var contentBox = document.getElementById('content-box');
+		}
+		contentBox.innerHTML= '';
+		contentBox.appendChild(photoPreload[currentPos]);
+		document.getElementById('mediaTitle').innerHTML = 'Фотография '+(currentPos+1)+' из '+photoQueue.length;
+		preloadPhoto(currentPos+1);
 	}
 	function showVideo(src,title)
 	{
@@ -100,5 +128,6 @@
 		contentBox.innerHTML= '';
 	}
 	var photoQueue = [];
+	var photoPreload = [];
 	var currentPos;
 	window.onload = function() {initMedia();}
